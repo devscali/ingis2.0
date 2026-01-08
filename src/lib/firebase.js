41b356ea84b442e69adc8482abe674ec4e1c1d23
Carlos Armando <149437182+devscali@users.noreply.github.com>
2026-01-07 18:10:03 -0800

    push

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYu8pGr9ceS4vLxCKm2i_QwXaMu-Eq0dU",
-  authDomain: "note-taker-ai-8bc38.firebaseapp.com",
-  projectId: "note-taker-ai-8bc38",
-  storageBucket: "note-taker-ai-8bc38.firebasestorage.app",
-  messagingSenderId: "491247346612",
-  appId: "1:491247346612:web:848d99e0f9331b897408e1",
-  measurementId: "G-K0N0PNGSQD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
